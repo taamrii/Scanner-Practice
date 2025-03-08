ab0b557d49f4089baa67a4457038a6382da5801b
Christelle Jeanlius <tjeanlius@gmail.com>
2025-03-08 01:29:36 -0500

    Create beginner2.java

---
+++ beginner2.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+ 
+ public class Main {
+ 
+     public static void main(String[] args){
+        //if statement = performs a block of code if its condition is true
+ 
+         Scanner scanner = new Scanner(System.in);
+ 
+         String name;
+         int age;
+         boolean isStudent;
+ 
+         System.out.print("Enter your name: ");
+         name = scanner.nextLine();
+ 
+         System.out.print("Enter your age: ");
+         age = scanner.nextInt();
+ 
+         System.out.print("Are you a student (true/false)? ");
+         isStudent = scanner.nextBoolean();
+ 
+         //GROUP 1
+         if (name.isEmpty()) {
+             System.out.println("You didnt enter your name!😭");
+         }
+         else{
+             System.out.println("Hello " + name + "!☺️" );
+         }
+ 
+         //GROUP 2
+         if(age >= 65){
+             System.out.println("You are a senior!👴🏼");
+         }
+         else if(age >= 18){
+             System.out.println("You are an adult!🧑🏽");
+         }
+         else if(age < 0) {
+             System.out.println("You haven't been born yet!👼🏼");
+         }
+         else if(age == 0){
+             System.out.println("You are a baby!👶🏽");
+         }
+         else{
+             System.out.println("You are a child!🧒");
+         }
+         //GROUP 3
+ 
+         if(isStudent){
+             System.out.println("You are a student!");
+         }
+         else{
+             System.out.println("You are NOT a student!");
+         }
+         scanner.close();
+     }
+ }
